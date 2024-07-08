7f3b09d4619f134bcdd438eeca5b929d873fb82b
Sam Trenter <sntrenter@gmail.com>
2024-07-08 17:31:53 -0500

    got dir page started

--- GDLViewer/dir/views.py
+++ GDLViewer/dir/views.py
@@ -5,17 +5,26 @@ from django.conf import settings
 from django.http import Http404
 from django.http import HttpResponse
 
+def list_files(path):
+    files = []
+    for file in os.listdir(path):
+        if os.path.isfile(os.path.join(path, file)):
+            files.append(file)
+    return files
+
 def display_directory(request, url):
-    #base_dir = settings.BASE_DIR / "your_base_directory"  # Set this to your base directory
-    #directory_path = base_dir / url
-
-    #if not os.path.exists(directory_path) or not os.path.isdir(directory_path):
-    #    raise Http404("Directory does not exist")
-
-    #directory_contents = os.listdir(directory_path)
-    #return render(request, 'your_template.html', {'directory_contents': directory_contents, 'directory_path': url})
+    path = os.path.join(settings.MEDIA_ROOT, url)
+    path = "http://127.0.0.1:4444/behance/Malina%20Cosmica%C2%AE/201130925%2080%20GEOMETRIC%20LOGOS/"
+    for f in list_files( url[1:]):
+        print(f)
+    print(path)
     print(url)
-    return HttpResponse("Hello, world. You're at dir display_directory.: " + url)
+    test_img = path + list_files( url[1:])[0]
+    print()
+    print("test_img: ", test_img)
+    print()
+    testtxt = f"<img src={test_img} >"
+    return HttpResponse("Hello, world. You're at dir display_directory.: " + url + testtxt)
 
 def index(request):
     return HttpResponse("Hello, world. You're at the dir index.") 
